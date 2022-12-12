6b2f45cd84902e63437e80c5777ed5104e5560b9
malikabdulhayee7 <99710069+malikabdulhayee7@users.noreply.github.com>
2022-12-12 18:52:30 +0500

    ddhj

--- 2nd.py
+++ 2nd.py
@@ -1,1 +1,7 @@
-#testing child branch for learning ds
+# if one false then false othervise true with two true then true
+
+album_year = 1983
+if(album_year < 1979) and (album_year > 1990):
+    print("The album was made in the 80's")
+
+
